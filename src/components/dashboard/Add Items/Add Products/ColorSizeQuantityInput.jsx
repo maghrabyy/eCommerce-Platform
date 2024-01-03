e79a330083deab99c8d3016688776eecddbfa45a
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-01-03 19:34:58 +0200

    fixing media queries styling

--- src/components/dashboard/Add Items/Add Products/ColorSizeQuantityInput.jsx
+++ src/components/dashboard/Add Items/Add Products/ColorSizeQuantityInput.jsx
@@ -32,36 +32,36 @@ export const ColorSizeQuantityInput = ({submitColorCallbk,darkBg})=>{
         }
     }
     return(
-        <div className={`flex flex-col xl:flex-row gap-2 xl:gap-0 justify-between p-2 xl:p-4 color-size-qty border-2 ${darkBg? 'border-gray-500' : 'border-gray-700'}  rounded-lg`}>
-            <div className="colorSizeQty-input flex flex-col xl:flex-row flex-wrap gap-2">
+        <div className={`flex flex-col xl:flex-row gap-2 xl:gap-0 justify-between p-2 xl:p-4 color-size-qty border-2 ${darkBg? 'border-gray-500' : 'border-gray-700'} rounded-lg`}>
+            <div className="colorSizeQty-input flex flex-col xl:flex-row gap-2">
                 <div className='prod-color flex flex-col'>
                     <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>Product Color</label>
                     <input type="text" className='inpt' placeholder="Enter the product color." value={prodColor} onChange={e=>setProdColor(e.target.value)} />
                 </div>
-                <div className='size-qty flex gap-1 xl:gap-2'>
+                <div className='size-qty flex flex-wrap justify-center gap-1 xl:gap-2'>
                     <div className='xs-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>XS</label>
-                        <input type="number" className='inpt w-12' max="99"  value={xsQty} onChange={e=>setXSQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99"  value={xsQty} onChange={e=>setXSQty(e.target.value)} />
                     </div>
                     <div className='s-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>S</label>
-                        <input type="number" className='inpt w-12' max="99"  value={sQty} onChange={e=>setSQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99"  value={sQty} onChange={e=>setSQty(e.target.value)} />
                     </div>
                     <div className='m-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>M</label>
-                        <input type="number" className='inpt w-12' max="99"  value={mQty} onChange={e=>setMQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99"  value={mQty} onChange={e=>setMQty(e.target.value)} />
                     </div>
                     <div className='l-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>L</label>
-                        <input type="number" className='inpt w-12' max="99"  value={lQty} onChange={e=>setLQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99"  value={lQty} onChange={e=>setLQty(e.target.value)} />
                     </div>
                     <div className='xl-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>XL</label>
-                        <input type="number" className='inpt w-12' max="99"  value={xlQty} onChange={e=>setXLQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99"  value={xlQty} onChange={e=>setXLQty(e.target.value)} />
                     </div>
                     <div className='xxl-size flex flex-col items-center'>
                         <label className={`${darkBg? 'inpt-label-dark' :'inpt-label'}`}>XXL</label>
-                        <input type="number" className='inpt w-12' max="99" value={xxlQty} onChange={e=>setXXLQty(e.target.value)} />
+                        <input type="number" className='inpt center w-10 xl:w-12' max="99" value={xxlQty} onChange={e=>setXXLQty(e.target.value)} />
                     </div>
                 </div>
                 <div className="imgs flex xl:flex-col xl:items-start gap-2 items-center">
--- src/pages/Dashboard/Products/BrandsCategoryPage.jsx
+++ src/pages/Dashboard/Products/BrandsCategoryPage.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom"
 import { Panel } from "../../../components/util/Panel"
 import { DashboardContent } from "../../../components/dashboard/Dashcontent"
 import { faShirt } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from "react-router-dom";
 
 export const BrandsCategoryPage = ()=>{
+    const navigate = useNavigate();
     return <DashboardContent className={'flex flex-col xl:flex-row justify-center gap-4 px-4'} title={'Products'} icon={faShirt}>
-            <Panel>
-                <Link className="font-semibold text-5xl px-6" to={'categories'}>Categories</Link>
+            <Panel onClick={()=>navigate('categories')}>
+                <div className="font-semibold text-5xl px-6" >Categories</div>
            </Panel>
-           <Panel>
-                <Link className="font-semibold text-5xl px-6" to={'brands'}>Brands</Link>
+           <Panel onClick={()=>navigate('brands')}>
+                <div className="font-semibold text-5xl px-6" >Brands</div>
            </Panel>
     </DashboardContent>
 }
