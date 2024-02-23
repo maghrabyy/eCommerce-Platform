b3bccfbcc0e117ed3bf47a0e0dc832cf67e4b722
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-02-23 22:33:54 +0200

    scrollTo Top when route change

--- src/components/dashboard/Dashcontent.jsx
+++ src/components/dashboard/Dashcontent.jsx
@@ -1,12 +1,20 @@
 import { PrimaryNavbar } from "./Navbar";
+import { useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
+
 export const dashcontentRef = { current: null };
 
 export const DashboardContent = ({icon,title,showSearchInput,className,children})=>{
     const navbarHeight = '85px'
+    const location = useLocation().pathname;
+    const contentRef = useRef();
+    useEffect(()=>{
+        contentRef.current.scrollTo(0,0);
+    },[location])
     return (
         <div ref={dashcontentRef} className={`dashboard-content  bg-white `}>
             <PrimaryNavbar icon={icon} title={title} showSearchInput={showSearchInput} />
-            <div style={{marginTop:navbarHeight}} className={`p-4 h-[calc(100vh-85px)] overflow-auto ${className}`}>
+            <div ref={contentRef} style={{marginTop:navbarHeight}} className={`p-4 h-[calc(100vh-85px)] overflow-auto ${className}`}>
                 { children}
             </div>
         </div>
