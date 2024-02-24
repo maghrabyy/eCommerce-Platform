598779639450387b1e69322747e896afcbf53155
Maghrabyy <mahmoud.elmaghraby11@gmail.com>
2024-02-24 19:16:20 +0200

    update login ui

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,8 +12,9 @@ export const Login = ()=>{
     const [password,setPassword] = useState('');
     return (
         <div className={`login w-5/6 sm:w-4/6 md:w-3/6 lg:w-2/6`}>
-          <form className='flex flex-col px-4 py-3 bg-white rounded-lg shadow-lg'>
-            <div className="login-input py-2 flex flex-col">
+          <form className='flex flex-col px-4 py-3 bg-white rounded-lg shadow-lg relative'>
+            <div className="login-title absolute top-0 left-0 text-white sm:text-3xl text-2xl text-center font-bold bg-slate-700 shadow-md shadow-slate-900 rounded-b-2xl py-6 w-full">Login as administrator</div>
+            <div className="login-input py-2 flex flex-col pt-24">
               <label className='inpt-label'>Username</label>
               <input type="text" autoComplete="username"  value={username} onChange={e=>setUsername(e.target.value)} autoFocus placeholder="Enter your username." required className='inpt' />
             </div>
